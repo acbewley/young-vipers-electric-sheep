f49d85de4d0a414a66731989d4a35a1ae39acff8
Heather Smith <halexsmith86@gmail.com>
2021-03-27 12:47:21 -0400

    Added /signup route to homeroutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -13,6 +13,16 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/signup", async (req, res) => {
+  try {
+    res.render("signup", {
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get("/profile", withAuth, async (req, res) => {
   try {
     const userData = await User.findByPk(req.session.user_id, {
