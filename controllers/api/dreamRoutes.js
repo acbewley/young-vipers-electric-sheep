907eb8f4f215a2d770ab93f9e9a7c103bd5b6547
acbewley <acbewley13@gmail.com>
2021-03-30 19:01:46 -0400

    edited dream put route

--- controllers/api/dreamRoutes.js
+++ controllers/api/dreamRoutes.js
@@ -16,19 +16,24 @@ router.post('/', withAuth, async (req, res) => {
 });
 
 router.put('/:id', withAuth, (req, res) => {
-    Dream.update(
-        {
-            subject: req.body.subject,
-            body: req.body.body
-        },
-        {
-            where: {
-                id: req.params.id,
-                user_id: req.session.user_id
+    try {
+        Dream.update(
+            {
+                subject: req.body.subject,
+                body: req.body.body
+            },
+            {
+                where: {
+                    id: req.params.id,
+                    user_id: req.session.user_id
+                }
             }
-        }
-    )
-})
+        )
+        res.status(200).json({ message: 'Success!' })
+    } catch (err) {
+        res.status(500).json(err);
+    }  
+});
 
 router.delete('/:id', withAuth, async (req, res) => {
     try {
